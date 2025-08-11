75edadf035285782e13134ebbfe805f22e0d23b7
Mostafa <mstfseyedi@gmail.com>
2025-08-11 11:27:35 +0200

    Deleted junk file

--- Final_Project/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-    def show_tasks(self):
-        if not self.tasks:
-            print("📭 No tasks found!")
-        else:
-            print("\n📋 To-Do List:")
-            print(f"{'No.':<4} {'Name':<20} {'Description':<40} {'Priority':<10}")
-            print("-" * 80)  # divider line
-            for i, task in enumerate(self.tasks, start=1):
-                print(f
